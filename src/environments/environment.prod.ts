d1d802847dad9772022dee7bf86a7810a31fbc05
gonzanav <gonzanaes3@gmail.com>
2023-04-27 02:29:06 -0300

    Ajustes frontend
    Ajustes para entrega final

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  firebase: {
+    projectId: 'gn-ap-frontend',
+    appId: '1:895734139058:web:6e9f296e22ed212e3d21ab',
+    storageBucket: 'gn-ap-frontend.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDjatM7nI830FWXCcDK5dmswqElrDqwGLA',
+    authDomain: 'gn-ap-frontend.firebaseapp.com',
+    messagingSenderId: '895734139058',
+  },
+  production: true,
+
+  URL: "https://gn-portfolio-back.onrender.com/",
+
+}
